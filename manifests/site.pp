2ff4513c8c29a7ddde9dc161ee815237011424b9
stevekelner <stevekellner@hotmail.com>
2016-06-28 13:19:48 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,11 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+  
+  file { '/etc/motd':
+   ensure => file,
+   owner => 'root',
+   group => 'root',
+      content => 'How to use travic-ci' \n',
+   }
 }
