c3ebd926093142d3335eae37dfe32aa353ef27e1
stevekelner <stevekellner@hotmail.com>
2016-06-29 15:07:17 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,11 +1,11 @@
 class nginx {
-package { 'nginx':
-ensure => present,
  File {
   owner => 'root',
   group => 'root',
   mode => '0644',
   }
+package { 'nginx':
+ ensure => present,
 }
 file { ['/var/www', '/etc/nginx/conf.d']:
 ensure => directory,
