1f720a367792b7c91b17a7288a972c8e31614d71
stevekelner <stevekellner@hotmail.com>
2016-06-30 13:50:41 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -50,6 +50,9 @@ node default {
   #   class { 'my_class': }
   #notify { "Hello, my name is ${::hostname}": }
   
+  $message = hiera('message')
+  notify { $message: }
+  
   #file { '/etc/motd':
    #ensure => file,
    #owner => 'root',
